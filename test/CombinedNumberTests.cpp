f63de8ba4bdfc5215f0810ef81748acfaf2abd54
PrzemekB <przemb@yahoo.com>
2019-02-11 17:35:08 +0100

    First two tests added

--- test/CombinedNumberTests.cpp
+++ test/CombinedNumberTests.cpp
@@ -1,11 +1,28 @@
 #include <gtest/gtest.h>
 #include "CombinedNumber.hpp"
+#include <list>
 
 struct CombinedNumberTests : public ::testing::Test
 {
+    CombinedNumber cn;
 };
 
 TEST_F(CombinedNumberTests, assertThatFalseIsTrue)
 {
-    ASSERT_TRUE(false);
+    ASSERT_TRUE(true);
 }
+
+TEST_F(CombinedNumberTests, whenEmptyListIsGivenReturnEmptyString)
+{
+    //std::list<unsigned int> l={};
+
+    auto result = cn.constructGreatestNumber({});
+    ASSERT_EQ(result, "");
+}
+
+TEST_F(CombinedNumberTests, whenOneIsGivenStringOneIsReturned)
+{
+    auto result = cn.constructGreatestNumber({1});
+    ASSERT_EQ(result, "1");
+}
+
